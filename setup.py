af0d80d8cc7b9004b526618e9825f8d42d05c026
Chong <chong.shao@mail.utoronto.ca>
2022-07-21 19:14:23 -0400

    cc install

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import numpy
 setup(
   name = 'OCAT',
   packages = ['OCAT'],
-  version = '0.1.88',
+  version = '0.1.881',
   license='MIT',
   description = 'A new single-cell analytics framework',
   author = '',
